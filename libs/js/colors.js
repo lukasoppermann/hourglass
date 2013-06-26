e4e232cbc04ebb0037bc4775e69f8233952ae157
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-06-26 22:45:14 +0200

    adding prototype

---
+++ libs/js/colors.js
@@ -0,0 +1,121 @@
+var pad = function(num, totalChars) {
+    var pad = '0';
+    num = num + '';
+    while (num.length < totalChars) {
+        num = pad + num;
+    }
+    return num;
+};
+
+// Ratio is between 0 and 1
+var changeColor = function(color, ratio, darker) {
+    // Trim trailing/leading whitespace
+    color = color.replace(/^\s*|\s*$/, '');
+
+    // Expand three-digit hex
+    color = color.replace(
+        /^#?([a-f0-9])([a-f0-9])([a-f0-9])$/i,
+        '#$1$1$2$2$3$3'
+    );
+
+    // Calculate ratio
+    var difference = Math.round(ratio * 256) * (darker ? -1 : 1),
+        // Determine if input is RGB(A)
+        rgb = color.match(new RegExp('^rgba?\\(\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '\\s*,\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '\\s*,\\s*' +
+            '(\\d|[1-9]\\d|1\\d{2}|2[0-4][0-9]|25[0-5])' +
+            '(?:\\s*,\\s*' +
+            '(0|1|0?\\.\\d+))?' +
+            '\\s*\\)$'
+        , 'i')),
+        alpha = !!rgb && rgb[4] != null ? rgb[4] : null,
+
+        // Convert hex to decimal
+        decimal = !!rgb? [rgb[1], rgb[2], rgb[3]] : color.replace(
+            /^#?([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])([a-f0-9][a-f0-9])/i,
+            function() {
+                return parseInt(arguments[1], 16) + ',' +
+                    parseInt(arguments[2], 16) + ',' +
+                    parseInt(arguments[3], 16);
+            }
+        ).split(/,/),
+        returnValue;
+
+    // Return RGB(A)
+    return !!rgb ?
+        'rgb' + (alpha !== null ? 'a' : '') + '(' +
+            Math[darker ? 'max' : 'min'](
+                parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+            ) + ', ' +
+            Math[darker ? 'max' : 'min'](
+                parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+            ) + ', ' +
+            Math[darker ? 'max' : 'min'](
+                parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+            ) +
+            (alpha !== null ? ', ' + alpha : '') +
+            ')' :
+        // Return hex
+        [
+            '#',
+            pad(Math[darker ? 'max' : 'min'](
+                parseInt(decimal[0], 10) + difference, darker ? 0 : 255
+            ).toString(16), 2),
+            pad(Math[darker ? 'max' : 'min'](
+                parseInt(decimal[1], 10) + difference, darker ? 0 : 255
+            ).toString(16), 2),
+            pad(Math[darker ? 'max' : 'min'](
+                parseInt(decimal[2], 10) + difference, darker ? 0 : 255
+            ).toString(16), 2)
+        ].join('');
+};
+var lighterColor = function(color, ratio) {
+    return changeColor(color, ratio, false);
+};
+var darkerColor = function(color, ratio) {
+    return changeColor(color, ratio, true);
+};
+
+// Use
+// var darker = darkerColor('rgba(80, 75, 52, .5)', .2);
+// var lighter = lighterColor('rgba(80, 75, 52, .5)', .2);
+
+var colors = function(baseColor){
+	var _item = $('.task'),
+		itemCount = _item.size(),
+		itemsCount = itemCount,
+		steps = Math.floor(itemCount/2),
+		extraStep = (itemCount/2 == Math.floor(itemCount/2)) ? 0 : 1,
+		colors = {};
+		
+		for( i = 0; steps >= i; ++i )
+		{
+			itemCount = itemCount - 1;
+			colors[itemCount] = lighterColor(baseColor,(i*3)/100);
+			_item.eq(steps-i).css('backgroundColor',colors[itemCount]);
+			
+			
+			
+			if(steps == i)
+			{
+				_item.eq(i+1).css('backgroundColor',baseColor);
+				itemCount = itemCount - (1 - extraStep);
+			}
+		}
+		
+		var outerI = 0;
+		
+		for( i = 1; steps >= i; ++i )
+		{
+			itemCount = itemCount - 1;
+			colors[itemCount] = darkerColor(baseColor,(i*3)/100);
+			_item.eq(steps+i).css('backgroundColor',colors[itemCount]);
+			outerI = i;
+		}
+		// 
+		// $('.tasks').find('lh').css({'backgroundColor':colors[0]});
+		$('#chrome').css({'backgroundColor':colors[0],'borderColor':'transparent transparent '+colors[0]+' transparent'});
+}		
---
+++ libs/js/javascript.js
@@ -0,0 +1,83 @@
+var _tasks = $('.tasks'),
+		_task = _tasks.find('.task'),
+		baseColor = $('.task').first().css('background-color');
+		
+// run colors function
+colors(baseColor);
+
+// sortable
+$(function() {
+	var open = false;
+   _tasks.sortable({
+     placeholder: "sortable-placeholder",
+		 forcePlaceholderSize: "forcePlaceholderSize",
+		 axis: "y",
+		 containment: "parent",
+		 distance: 60,
+		 items: '.task',
+		 start: function(event, ui) {
+			 if(ui.item.hasClass('open'))
+			 {
+					ui.helper.find('.body').parent().removeClass('open');
+					open = true;
+				}
+			 ui.helper.height(ui.helper.find('.head').height());
+			 $('.sortable-placeholder').height(ui.helper.find('.head').height());
+		 },
+		 stop: function(event, ui) {
+			 colors(baseColor);
+			 if(open === true)
+			 {
+			 	ui.item.find('.body').parent().addClass('open');
+			 }
+		 }
+   });
+   _tasks.disableSelection();
+	 
+ });
+// editable in sortable
+ $('.editable').each( function(){
+     $(this)[0].onmousedown = function() {
+         this.focus();
+     };
+ });
+// open / close item
+_tasks.on('click','.open-arrow', function()
+{
+	var item = $(this).parents('.task');
+	
+	if( item.hasClass('open') )
+	{
+		item.removeClass('open');
+	}
+	else
+	{
+		_tasks.find('.open').removeClass('open');
+		item.addClass('open');
+	}
+	
+});
+// Check subtask
+_tasks.on('click', '.subtask .checkbox', function(){
+	var _item = $(this).parent('.subtask');
+	
+	if( _item.hasClass('checked') )
+	{
+		_item.removeClass('checked');
+	}
+	else
+	{
+		var _archive = _item.parents('.body').find('.subtask-archive');
+		// add class
+		_item.addClass('checked');
+		// animate item
+		var archiveOff = _archive.offset();
+		var itemOff = _item.offset();
+		var moveTop = archiveOff.top - itemOff.top;
+		_item.css({'position':'absolute','top':itemOff.top}).after('<li class="subtask-hide" style="display: block; height:'+_item.outerHeight()+'px" />').animate({'top':'+='+moveTop, 'opacity':0}, 500);
+		// animate hide task space
+		$('.subtask-hide').animate({'height':0}, 500, function(){
+			$(this).remove();
+		});
+	}
+});
