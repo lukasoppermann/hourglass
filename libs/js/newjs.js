b19594ea3465cb0d553cff31fd5a92083a419d0d
Lukas Oppermann <oppermann.lukas@googlemail.com>
2013-07-09 14:12:08 +0200

    working on projects

--- libs/js/newjs.js
+++ libs/js/newjs.js
@@ -29,32 +29,49 @@ $('#head_projects').on('click', function(){
 		$(this).find('.title').removeClass('active');
 		
 		$('#projects_view').find('.view-content').find('lh').removeClass('active');
+		$('#projects_view').find('.view-content').find('li').addClass('active');
 	}
 	else
 	{
 		$(this).find('.title').addClass('active');
 		
 		$('#projects_view').find('.view-content').find('lh').addClass('active');
+		$('#projects_view').find('.view-content').find('li').removeClass('active');
 	}
 	
 });
 //---------------------
 // projects connected groups
-	$('#projects_view').find('.view-content').sortable({
-		placeholder: "project-sort-placeholder",
-		forcePlaceholderSize: "forcePlaceholderSize",
-		axis: "y",
-		containment: "parent",
-		distance: 5,
-		items: '.group',
-		sort: function(){
-		$(window).on("keydown", function( event ){
-			if(event.keyCode == 27)
-			{
-				$('#projects_view').find('.view-content').sortable( "cancel" );
-			}
-		});
+$('#projects_view').find('.view-content').sortable({
+	placeholder: "project-sort-placeholder",
+	forcePlaceholderSize: "forcePlaceholderSize",
+	axis: "y",
+	containment: "parent",
+	distance: 5,
+	items: '.group',
+	sort: function(){
+	$(window).on("keydown", function( event ){
+		if(event.keyCode == 27)
+		{
+			$('#projects_view').find('.view-content').sortable( "cancel" );
 		}
 	});
+	}
+});
+//---------------------
+// play pause
+$('.circle-wrapper').on('click', function()
+{
+	if( $(this).hasClass('active') )
+	{
+		$('.circle-wrapper').removeClass('active');
+	}
+	else
+	{
+		$('.circle-wrapper').removeClass('active');
+		$(this).addClass('active');
+	}
+	
+});
 // close jquery	
 });
